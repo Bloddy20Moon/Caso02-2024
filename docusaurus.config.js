26a442d8393072974446781537b341922f86456d
Bloddy20Moon <adrian0212alvarado@gmail.com>
2024-02-03 18:32:37 -0500

    Sesion03

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://bloddy20moon.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Caso02-2024',
+  baseUrl: '/Sesion03/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
